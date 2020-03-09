e30a35b5ec495f333bce1a4b7a0d1869938d180d
HuangZengPei <zphuang96@163.com>
2020-03-09 10:45:08 +0800

    O(1)时间删除链表节点

---
+++ 12在O(1)时间删除节点.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self):
+        self.value = None
+        self.next_node = None
+        
+class Solution:
+    def delete_node(self,head_node,del_node):
+        if not head_node and del_node:
+            return False
+
+        if del_node.next_node:             #删除的节点不是尾节点
+            del_next = del_node.next_node
+            del_node.value = del_next.value
+            del_node.next_node = del_next.next_node
+            del_next.value = None
+            del_next.next_node = None
+        
+        elif del_node == head_node:
+            del_node == None
+            head_node == None
+        else:
+            node = head_node
+            while node.next_node != del_node:
+                node = node.next_node
+            node.next_node = None
+            del_node = None
+        return head_node
