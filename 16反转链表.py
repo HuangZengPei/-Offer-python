48fbc0ed2155de8d7826ee6e3d4ac474a6f9628b
HuangZengPei <zphuang96@163.com>
2020-03-10 16:25:11 +0800

    反转链表，注意头节点的next为空

---
+++ 16反转链表.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        # write code here
+        if not pHead:return None
+        if not pHead.next:return pHead
+        p1 = pHead
+        p2 = pHead.next
+        pHead.next = None
+        while p2.next:
+            p3 = p2.next
+            p2.next = p1
+            p1 = p2
+            p2 = p3
+        p2.next = p1
+        return p2
